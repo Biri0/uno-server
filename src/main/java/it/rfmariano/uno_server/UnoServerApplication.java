a22f174e99c3fcf86d9da707b109f5e9574d1c76
Mariano Riefolo <rfmariano@protonmail.com>
2025-09-18 14:42:13 +0200

    initial commit

---
+++ src/main/java/it/rfmariano/uno_server/UnoServerApplication.java
@@ -0,0 +1,13 @@
+package it.rfmariano.uno_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnoServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnoServerApplication.class, args);
+	}
+
+}
